652f6dcfc9db7942276122880437cb2aa5ec8571
Katy Fry <katyvfry@gmail.com>
2023-02-01 16:46:34 -0600

    add favicon

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="h-[60px] px-10 bg-green body-text text-pink flex flex-col items-center lg:flex-row lg:justify-between">
+        <div className="h-[50px] lg:h-[30px] px-10 bg-green body-text text-[12px] leading-[12px] text-pink flex flex-col items-center justify-center lg:flex-row lg:justify-between">
             <p>Created by Katy Fry</p>
             <p>Copyright 2023</p>
             <p>Find me on <a href="https://github.com/katherinevfry">Github</a></p>
