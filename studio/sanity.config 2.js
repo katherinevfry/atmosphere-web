730343ad23c418e29668eeb943f5ff0d4d37a267
Katy Fry <katyvfry@gmail.com>
2025-08-29 11:53:47 -0500

    sanity updates

---
+++ studio/sanity.config 2.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "sanity";
+
+export default defineConfig({
+  title: "atmosphere-studio",
+  projectId: "9h4vha0j",
+  dataset: "production",
+  plugins: [],
+  schema: {
+    types: [],
+  },
+});
